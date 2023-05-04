285c79c5c19879450a9ccceccce8d067e27a1389
PakaipitchL <Pakaipitch.l@gmail.com>
2023-05-04 12:03:15 +0700

    category page (รอจัดหน้า)

--- corgi/corgi/urls.py
+++ corgi/corgi/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('seller/', include('seller.urls')),
     path('', include("homepage.urls")),
+    path('buyer/', include('buyer.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
