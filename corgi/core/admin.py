39d6d25eeda3bb057c202796f8cde267d41bab42
PasutK <pcasut@gmail.com>
2023-05-06 00:49:38 +0700

    core update

---
+++ corgi/core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as CorgiUserAdmin
+from .models import User
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(CorgiUserAdmin):
+    add_fieldsets = (
+        (
+        None,
+        {
+            "classes": ("wide",),
+            "fields": ("username", "password1", "password2", "email", "first_name",
+                       "last_name", "phone", "address")
+        },
+        ),
+    )
---
+++ corgi/core/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import re
+from django.core.exceptions import ValidationError
+
+def email_validator(value):
+    pattern = r'@.+.chula.ac.th'
+    if not re.search(pattern, value):
+        raise ValidationError('Email address must end with chula.ac.th')
+    
+def phone_validator(value):
+    pattern = r'^\d{3}[-]?\d{3}[-]?\d{4}$'
+    if not re.search(pattern, value):
+        raise ValidationError('Invalid Phone number')
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, validators=[email_validator])
+    phone = models.CharField(max_length=10, validators=[phone_validator])
+    address = models.CharField(max_length=255)
