74d7d841a57b2e6c8a023e8df1ccb76ce2478871
PakaipitchL <Pakaipitch.l@gmail.com>
2023-05-05 14:55:55 +0700

    product listing screen (รอจัดหน้า)

--- corgi/seller/urls.py
+++ corgi/seller/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.sbase,name='seller-homepage'),
     path('register/', views.register_seller,name='seller-register'),
     path('login/', views.Slogin,name='seller-login'),
-    path('products/', views.seller_product, name='eller_product'),
+    path('products/', views.seller_product, name='seller_product'),
 ]
