299f7fb6d12c64c186b07fb5c42142542b12c096
PasutK <pcasut@gmail.com>
2023-05-06 17:20:33 +0700

    core.models change

--- corgi/core/models.py
+++ corgi/core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-import re
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.exceptions import ValidationError
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import re
 
 def email_validator(value):
     pattern = r'@.+.chula.ac.th'
@@ -33,26 +35,47 @@ class User(AbstractUser):
             self.role = self.base_role
             return super().save(*args, **kwargs)
         
+
 class CoreBuyerManager(BaseUserManager):
     def get_queryset(self, *args, **kwargs):
         results = super().get_queryset(*args, **kwargs)
         return results.filter(role=User.Role.BUYER)
-
 
 class CoreBuyer(User):
     base_role = User.Role.BUYER
     buyer = CoreBuyerManager()
     class META:
         proxy=True
+    def welcome(self):
+        return "Only for Buyers"
 
-class CoreSellerrManager(BaseUserManager):
+@receiver(post_save, sender=User)
+def create_buyer_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "BUYER":
+        BuyerProfile.objects.create(user=instance)
+
+class BuyerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    buyer_id = models.IntegerField(null=True)
+
+class CoreSellerManager(BaseUserManager):
     def get_queryset(self, *args, **kwargs):
         results = super().get_queryset(*args, **kwargs)
         return results.filter(role=User.Role.SELLER)
 
-
 class CoreSeller(User):
     base_role = User.Role.SELLER
-    buyer = CoreSellerrManager()
+    seller = CoreSellerManager()
     class META:
         proxy=True
+    def welcome(self):
+        return "Only for Sellers"
+
+@receiver(post_save, sender=User)
+def create_seller_profile(sender, instance, created, **kwargs):
+    if created and instance.role == "SELLER":
+        SellerProfile.objects.create(user=instance)
+
+class SellerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    seller_id = models.IntegerField(null=True)
