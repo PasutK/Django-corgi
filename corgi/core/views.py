f3c702626ffbbfe1ec9032598d9cf1162a9f75ad
PasutK <pcasut@gmail.com>
2023-05-07 11:04:01 +0700

    Register and Login System Done

--- corgi/core/views.py
+++ corgi/core/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .forms import FormRegistration
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import *
+from homepage.views import homepage
 
-# Create your views here.
+def userlogin(request):
+    if request.method == "POST":
+        username =  request.POST['username']
+        password = request.POST["password"]
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Login successful. Enjoy Corgi!")
+            return redirect(userprofile)
+        else:
+            messages.success(request,"Wrong Username or Password, please try again")
+            return redirect(userlogin)
+    else:
+        return render(request, 'login.html', {})
+
+def userlogout(request):
+    logout(request)
+    return render(request, "logout.html", {})
+
+# class UserRegisterView(CreateView):
+#     form_class = FormRegistration
+#     template_name = "register.html"
+#     success_url = reverse_lazy("core:profile")
+
+#     def form_valid(self, form):
+#         response = super().form_valid(form)
+#         form.save()
+#         return response
+
+
+def userregister(request):
+    form = FormRegistration(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password1"]
+        user = authenticate(username=username,password=password)
+        login(username,password)
+        return redirect(userprofile)
+    return render(request, "register.html", {'form':form})
+
+#         registered_username = request.POST["username"]
+#         registered_password = request.POST["password1"]
+#         registered_email = request.POST["email"]
+#         registered_firstname = request.POST["first_name"]
+#         registered_lastname = request.POST["last_name"]
+
+#         user = User.objects.create_user(username=registered_username,
+#                                         password=registered_password,
+#                                         first_name=registered_firstname,
+#                                         last_name=registered_lastname,
+#                                         email=registered_email)
+#         user.save()
+#         seller_profile = User.objects.create()
+#         messages.success(request, "Your store has created!")
+#         return redirect('/seller/products') 
+#     else:  
+#         form = FormRegistration()
+#     return render(request, 'Sregister.html', {'form': form })
+
+@login_required
+def userprofile(request):
+    return render(request, "profile.html", {})
--- corgi/seller/urls.py
+++ corgi/seller/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import*
+from .views import *
 
 urlpatterns = [
     path('', views.sbase,name='seller-homepage'),
