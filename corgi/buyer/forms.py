32f7c549bdc787787a22a3181879aa4f33720d1b
PasutK <pcasut@gmail.com>
2023-05-08 00:26:02 +0700

    New branch

--- corgi/buyer/forms.py
+++ corgi/buyer/forms.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django import forms
-from .models import Cartitems
+from .models import *
 
 class AddtoCart(forms.ModelForm):
     class Meta:
-        model = Cartitems
+        model = Cartitem
         fields = ["amount"]
