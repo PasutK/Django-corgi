efb88a57421bad4117ccb30688a54d0b01062c7f
PakaipitchL <Pakaipitch.l@gmail.com>
2023-05-10 13:57:45 +0700

    botton

--- corgi/seller/models.py
+++ corgi/seller/models.py
@@ -6,19 +6,6 @@ from django.contrib.auth.models import Permission
 from django.contrib.auth.models import Group
 from django.contrib.auth.models import AbstractUser
 import re
-
-# class SellerPermissions:
-#     CAN_ADD_PRODUCT = 'can_add_product'
-
-# def create_seller_permissions():
-#     permission = Permission.objects.create(
-#         codename=SellerPermissions.CAN_ADD_PRODUCT,
-#         name='Can add products to marketplace',
-#         content_type_id=0)
-#     return permission
-
-
-# sellers_group = Group.objects.create(name='Sellers')
     
 class Seller(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,max_length=100, null=False, blank=False, on_delete=models.CASCADE)
@@ -28,7 +15,6 @@ class Seller(models.Model):
     qrcode_image = models.ImageField(upload_to="seller/media/qrcode/")
     last_update = models.DateTimeField(auto_now=True)
     store_phone = models.CharField(max_length=10, default= None, null=True)
-    # user = models.ForeignKey(User) --> fix later
 
     @staticmethod
     def get_all_sellers():
